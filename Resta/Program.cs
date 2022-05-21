6a3bd99fc5ad9672df9ac42fbf8548b276ff5fd7
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 00:50:20 -0700

    Agregar archivos de proyecto.

---
+++ Resta/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;
+            int num2 = 8;
+
+            Console.WriteLine(num1-num2);
+            Console.ReadKey();
+
+        }
+    }
+}
